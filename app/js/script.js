f9392c6505f7d4bcab4b7c2c0313bdf5bee9ff94
malekak44 <malekak44@gmail.com>
2021-10-12 12:42:27 +0600

    mobile navigation done

---
+++ app/js/script.js
@@ -0,0 +1,21 @@
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const fadeElems = document.querySelectorAll('.has-fade');
+
+btnHamburger.addEventListener('click', function (e) {
+    e.preventDefault();
+    if (header.classList.contains('open')) { // Close Hamburger Menu
+        header.classList.remove('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+        });
+    }
+    else { // Open Hamburger Menu
+        header.classList.add('open');
+        fadeElems.forEach(function (element) {
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+        });
+    }
+});
